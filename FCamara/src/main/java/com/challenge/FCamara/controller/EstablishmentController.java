37821483262e4c23e1474957447d4857cfbfa801
Pichau <igorof06@outlook.com>
2024-08-08 16:44:24 -0300

    implementation controller for Establishment

---
+++ FCamara/src/main/java/com/challenge/FCamara/controller/EstablishmentController.java
@@ -0,0 +1,52 @@
+package com.challenge.FCamara.controller;
+
+import com.challenge.FCamara.dtos.SaveEstablishmentDto;
+import com.challenge.FCamara.dtos.UpdateEstablishmentDto;
+import com.challenge.FCamara.entities.Establishment;
+import com.challenge.FCamara.service.EstablishmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/establishment")
+public class EstablishmentController {
+
+    private EstablishmentService establishmentService;
+
+    public EstablishmentController (EstablishmentService establishmentService) {
+        this.establishmentService = establishmentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveEstablishment(@RequestBody SaveEstablishmentDto request) {
+
+        establishmentService.saveEstablishment(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Establishment>> getAllEstablishments() {
+
+        var allEstablishments = establishmentService.getAllEstablishments();
+        return new ResponseEntity<>(allEstablishments, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateEstablishment(@PathVariable(name = "id") Long id,
+                                                    @RequestBody UpdateEstablishmentDto request) {
+
+        establishmentService.updateEstablishment(id, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEstablishment(@PathVariable(name = "id") Long id) {
+
+        establishmentService.deleteEstablishment(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
