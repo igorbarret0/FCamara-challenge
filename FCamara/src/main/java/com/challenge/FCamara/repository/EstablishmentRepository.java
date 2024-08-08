d7a37d7caa82329186f13c1bba979715cabb023c
Pichau <igorof06@outlook.com>
2024-08-08 11:31:09 -0300

    creation of repositories: Establishment and Vehicle

---
+++ FCamara/src/main/java/com/challenge/FCamara/repository/EstablishmentRepository.java
@@ -0,0 +1,9 @@
+package com.challenge.FCamara.repository;
+
+import com.challenge.FCamara.entities.Establishment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstablishmentRepository extends JpaRepository<Establishment, Long> {
+}
---
+++ FCamara/src/main/java/com/challenge/FCamara/repository/VehicleRepository.java
@@ -0,0 +1,9 @@
+package com.challenge.FCamara.repository;
+
+import com.challenge.FCamara.entities.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+}
